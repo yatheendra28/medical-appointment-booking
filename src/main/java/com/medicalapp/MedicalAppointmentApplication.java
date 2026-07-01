350a1c0655eec3eea9d5ff2db626b9371d0613f4
yatheendra28 <ryatheendra28@gmail.com>
2026-07-01 23:38:48 +0530

    Add main application class

---
+++ src/main/java/com/medicalapp/MedicalAppointmentApplication.java
@@ -0,0 +1,11 @@
+package com.medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentApplication.class, args);
+    }
+}
