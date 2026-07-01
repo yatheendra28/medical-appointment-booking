880be7260cf6fa0849e83892d60c7fc064cad3ad
yatheendra28 <ryatheendra28@gmail.com>
2026-07-01 23:42:05 +0530

    Add Appointment repository

---
+++ src/main/java/com/medicalapp/repository/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package com.medicalapp.repository;
+
+import com.medicalapp.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorId(Long doctorId);
+    List<Appointment> findByPatientEmail(String email);
+    List<Appointment> findByStatus(String status);
+}
